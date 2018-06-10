19ba5bcdce7f4c025187a93f4c7498d1e5763b49
alex_amorim <40036340+al3xamorim@users.noreply.github.com>
2018-06-10 09:08:49 -0300

    JS Alex
    Trabalho da faculdade

---
+++ JS - Alex/index.js
@@ -0,0 +1,39 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+function slider() {
+  document.getElementById('img').src = "imagens/port4.jpg"
+  setTimeout("slider2()", 3000)
+}
+function slider2() {
+  document.getElementById('img').src = "imagens/port5.jpg"
+  setTimeout("slider()", 3000)
+}
